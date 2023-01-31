3531bb51aa5b4fac5b66b48b5fb880f940c71301
Frank Richter <frank.richter@muv-siegmar.de>
2023-01-31 13:41:17 +0100

    downcase PK column from DB to match attribute keys

--- lib/odbc_adapter/schema_statements.rb
+++ lib/odbc_adapter/schema_statements.rb
@@ -92,7 +92,7 @@ module ODBCAdapter
       stmt   = @connection.primary_keys(native_case(table_name.to_s))
       result = stmt.fetch_all || []
       stmt.drop unless stmt.nil?
-      result[0] && result[0][3]
+      result[0] && result[0][3]&.downcase
     end
 
     def foreign_keys(table_name)
